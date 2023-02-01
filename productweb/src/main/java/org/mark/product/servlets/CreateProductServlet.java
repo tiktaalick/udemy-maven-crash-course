0bce9103cf8daad19a5f972dc212899ace945401
tiktaalick <blogfeedser@gmail.com>
2023-02-01 15:22:32 +0100

    Up to section 6: multi-module project.

---
+++ productweb/src/main/java/org/mark/product/servlets/CreateProductServlet.java
@@ -0,0 +1,10 @@
+package org.mark.product.servlets;
+
+import org.mark.product.dto.Product;
+
+public class CreateProductServlet {
+
+    protected void doPost(String something) {
+        Product product = new Product();
+    }
+}
